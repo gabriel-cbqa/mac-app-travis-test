e1b958bcd201821325337252c1960b8bd68c4b24
gabriel-cbqa <38506642+gabriel-cbqa@users.noreply.github.com>
2018-05-03 11:38:05 -0500

    Add files via upload

---
+++ Datos.swift
@@ -0,0 +1,20 @@
+//
+//  Datos.swift
+//  AppEjercicio16
+//
+//  Created by ISSC411 on 12/04/18.
+//  Copyright © 2018 GLL. All rights reserved.
+//
+
+import Cocoa
+
+class Datos: NSObject
+{
+    var Titulo:String = ""
+    var Description: String = ""
+    var Imagen: String = ""
+    var Detalle: String = ""
+    var id:Int=0
+    
+}
+
